72ad9daa9dacd51e39c23cadb2b45b78a496ed84
Vladyslav-Hrossu <vgrossu31@gmail.com>
2020-11-12 13:51:02 +0200

    #4 Updated calculator page, initiated firebase hosting

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AbstractFormComponent } from '@shared/classes/abstract-form-component';
+import { UKRAINE_REGIONS_DISPLAY } from '@models/ukraine-regions.const';
 
 @Component({
     selector: 'app-calculator',
@@ -9,6 +10,7 @@ import { AbstractFormComponent } from '@shared/classes/abstract-form-component';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CalculatorComponent extends AbstractFormComponent implements OnInit {
+    uaDisplayRegions = UKRAINE_REGIONS_DISPLAY;
     panelTypeOptions = [
         { name: 'Crystalline Silicone', value: 'crystallineSilicone' },
         { name: 'CIS', value: 'crystallineSilicone' },
@@ -19,8 +21,13 @@ export class CalculatorComponent extends AbstractFormComponent implements OnInit
         super();
     }
 
+    calculateSolarEnergy(): void {
+        console.log(this.form);
+    }
+
     protected initForm(): void {
         this.form = this.fb.group({
+            map: [ null, [ Validators.required ] ],
             power: [ null, [ Validators.required, Validators.min(0.1) ] ],
             panelsType: [ null, [ Validators.required ] ],
             efficiency: [ 100, [ Validators.required, Validators.min(1), Validators.max(100) ] ],
--- src/app/shared/components/ua-map/ua-map.component.ts
+++ src/app/shared/components/ua-map/ua-map.component.ts
@@ -1,27 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, forwardRef, OnInit, Self } from '@angular/core';
 import { UKRAINE_REGIONS, UKRAINE_REGIONS_DISPLAY } from '@models/ukraine-regions.const';
+import { NgOnDestroy } from '@core/services/ng-on-destroy.service';
+import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors } from '@angular/forms';
 
 @Component({
     selector: 'app-ua-map',
     templateUrl: './ua-map.component.html',
-    styleUrls: [ './ua-map.component.scss' ]
+    styleUrls: [ './ua-map.component.scss' ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [
+        NgOnDestroy,
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => UaMapComponent),
+            multi: true
+        },
+        {
+            provide: NG_VALIDATORS,
+            useExisting: forwardRef(() => UaMapComponent),
+            multi: true
+        }
+    ]
 })
-export class UaMapComponent implements OnInit {
+export class UaMapComponent implements OnInit, ControlValueAccessor {
     uaRegions = UKRAINE_REGIONS;
     uaRegionsDisplay = UKRAINE_REGIONS_DISPLAY;
-    selectedRegion = null;
+    selectedRegion: UKRAINE_REGIONS = null;
 
-    constructor() {
+    constructor(@Self() private destroy$: NgOnDestroy) {
     }
 
     ngOnInit(): void {
     }
 
     onClick({ target: { id } }): void {
-        this.selectedRegion = +id;
+        if (+id) {
+            this.selectedRegion = +id;
+            this.onChange(this.selectedRegion);
+        }
     }
 
     isRegionSelected(regionId: UKRAINE_REGIONS): boolean {
         return regionId === this.selectedRegion;
+    }
+
+    writeValue(value: UKRAINE_REGIONS): void {
+        this.selectedRegion = value;
+    }
+
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    setDisabledState(isDisabled: boolean): void {
+    }
+
+    validate(c: AbstractControl): ValidationErrors | null {
+        return this.selectedRegion ? null : c.errors;
+    }
+
+    private onChange = (value: UKRAINE_REGIONS) => {
+    }
+
+    private onTouched = () => {
     }
 }
